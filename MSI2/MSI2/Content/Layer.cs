66d7adb217c8b71aaf9635b924b855b49d0d7788
SidLeniwiec <piotrmichalik17@wp.pl>
2017-04-18 22:27:59 +0200

    pierwsza konstrukcja sieci

---
+++ MSI2/MSI2/Content/Layer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSI2.Content
+{
+    public struct Layer
+    {
+        private int inputVal;
+        private int outputVal;
+        private int[,] values;
+
+        public int InputNeurons
+        {
+            get{
+                return inputVal;
+            }
+            set{
+                inputVal = value;
+            }
+        }
+        public int OutputNeurons
+        {
+            get{
+                return outputVal;
+            }
+            set{
+                outputVal = value;
+            }
+        }
+        public int[,] Values
+        {
+            get{
+                return values;
+            }
+            set{
+                values = value;
+            }
+        }
+
+        public Layer(int input, int output)
+        {
+            this.inputVal = input;
+            this.outputVal = output;
+            this.values = new int[input, output];
+        }
+    }
+}
---
+++ MSI2/MSI2/Content/Network.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSI2.Content
+{
+    public class Network
+    {
+        public int InputLength { get; set; }
+        public int Classes { get; set; }
+        public int HiddenLayers { get; set; }
+        public int[] NeuronPerLayer { get; set; }
+        public bool Bias { get; set; }
+        public int Seed { get; set; }
+        public int Iterations { get; set; }
+        public Layer[] Layers { get; set; }
+
+        public Network()
+        {
+
+        }
+        public Network(int inputLength, int classes, int hiddenLayers, int[] neuronPerLayer, bool bias, int seed, int iterations, Layer[] layers)
+        {
+            InputLength = inputLength;
+            Classes = classes;
+            HiddenLayers = hiddenLayers;
+            NeuronPerLayer = neuronPerLayer;
+            Bias = bias;
+            Seed = seed;
+            Iterations = iterations;
+            Layers = layers;
+        }
+    }
+}
